8a9633a306e43d552789d5f9f811e592f4f22512
mralj <nikola.mratinic@gmail.com>
2024-09-26 20:21:47 +0200

    added example how code in overrides would change

--- core/vm/contracts_rollup_overrides.go
+++ core/vm/contracts_rollup_overrides.go
@@ -3,7 +3,9 @@
 
 package vm
 
-import "math/big"
+import (
+	"math/big"
+)
 
 type RollupPrecompiledContractsOverrides struct {
 	l1SLoadGetLatestL1Block func() *big.Int
@@ -23,3 +25,16 @@ func getLatestL1BlockNumber(evm *EVM) func() *big.Int {
 		return evm.Context.BlockNumber
 	}
 }
+
+// [OVERRIDE]  getLatestL1BlockNumber
+// Each rollup should override this function so that it returns
+// correct latest L1 block number
+//
+// EXAMPLE 2
+// func getLatestL1BlockNumber(evm *EVM) func() *big.Int {
+// 	return func() *big.Int {
+// 		addressOfL1BlockContract := common.Address{}
+// 		slotInContractRepresentingL1BlockNumber := common.Hash{}
+// 		return evm.StateDB.GetState(addressOfL1BlockContract, slotInContractRepresentingL1BlockNumber).Big()
+// 	}
+// }
